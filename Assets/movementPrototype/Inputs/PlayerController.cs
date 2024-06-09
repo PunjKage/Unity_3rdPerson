e2a11b9fd1725fd95050863e063c61d39a67f464
Naman_Punj <punjkage@gmail.com>
2024-06-09 20:32:28 +0530

    Initial COmmit

---
+++ Assets/movementPrototype/Inputs/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Player playerInput;
+
+    private void Awake()
+    {
+        playerInput = new Player();
+    }
+
+    public void OnEnable()
+    {
+        
+    }
+
+    public void OnDisable()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
